a17b7dd803ae7912eaed6b600ca55f8fc13e0970
Игорь Чечет <ichechet@mail.ru>
2022-01-13 14:33:33 +0500

    Отдельно выводим данные тикера, его торговый счет и последнюю цену сделки

--- Examples/03 - Ticker.py
+++ Examples/03 - Ticker.py
@@ -7,20 +7,27 @@ if __name__ == '__main__':  # Точка входа при запуске это
 
     firmId = 'MC0063100000'  # Фирма
     classCode = 'TQBR'  # Класс тикера
-    secCode = 'GAZP'  # Тикер
+    secCode = 'SBER'  # Тикер
     
     # firmId = 'SPBFUT'  # Фирма
     # classCode = 'SPBFUT'  # Класс тикера
     # secCode = 'SiH2'  # Для фьючерсов: <Код тикера><Месяц экспирации: 3-H, 6-M, 9-U, 12-Z><Последняя цифра года>
 
-    # Данные тикера и его торговый счет
+    # Данные тикера
     securityInfo = qpProvider.GetSecurityInfo(classCode, secCode)["data"]
     print(f'Информация о тикере {classCode}.{secCode} ({securityInfo["short_name"]}):')
-    print(f'Валюта: {securityInfo["face_unit"]}')
-    print(f'Кол-во десятичных знаков: {securityInfo["scale"]}')
-    print(f'Лот: {securityInfo["lot_size"]}')
-    print(f'Шаг цены: {securityInfo["min_price_step"]}')
-    print(f'Торговый счет для тикера класса {classCode}: {qpProvider.GetTradeAccount(classCode)["data"]}')
+    print('Валюта:', securityInfo['face_unit'])
+    print('Кол-во десятичных знаков:', securityInfo['scale'])
+    print('Лот:', securityInfo['lot_size'])
+    print('Шаг цены:', securityInfo['min_price_step'])
+
+    # Торговый счет тикера
+    tradeAccount = qpProvider.GetTradeAccount(classCode)["data"]  # Торговый счет для класса тикера
+    print(f'Торговый счет для тикера класса {classCode}: {tradeAccount}')
+
+    # Последняя цена сделки
+    lastPrice = float(qpProvider.GetParamEx(classCode, secCode, 'LAST')['data']['param_value'])  # Последняя цена сделки
+    print('Последняя цена сделки:', lastPrice)
 
     # Выход
     qpProvider.CloseConnectionAndThread()  # Перед выходом закрываем соединение и поток QuikPy из любого экземпляра
