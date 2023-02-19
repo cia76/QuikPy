cd9a272fc9ce73dee825cc96b596f9a797b88efd
Игорь Чечет <ichechet@mail.ru>
2023-02-19 19:37:30 +0500

    Ответ от сервера

--- Examples/03 - Ticker.py
+++ Examples/03 - Ticker.py
@@ -14,12 +14,13 @@ if __name__ == '__main__':  # Точка входа при запуске это
     # secCode = 'SiH2'  # Для фьючерсов: <Код тикера><Месяц экспирации: 3-H, 6-M, 9-U, 12-Z><Последняя цифра года>
 
     # Данные тикера
-    securityInfo = qpProvider.GetSecurityInfo(classCode, secCode)["data"]  # Интерпретатор языка Lua, Таблица 4.21 Инструменты
-    print(f'Информация о тикере {classCode}.{secCode} ({securityInfo["short_name"]}):')  # Короткое наименование инструмента
-    print('Валюта:', securityInfo['face_unit'])  # Валюта номинала
-    print('Кол-во десятичных знаков:', securityInfo['scale'])  # Точность (количество значащих цифр после запятой)
-    print('Лот:', securityInfo['lot_size'])  # Размер лота
-    print('Шаг цены:', securityInfo['min_price_step'])  # Минимальный шаг цены
+    si = qpProvider.GetSecurityInfo(classCode, secCode)['data']  # Интерпретатор языка Lua, Таблица 4.21 Инструменты
+    print('Ответ от сервера:', si)
+    print(f'Информация о тикере {classCode}.{secCode} ({si["short_name"]}):')  # Короткое наименование инструмента
+    print('Валюта:', si['face_unit'])  # Валюта номинала
+    print('Кол-во десятичных знаков:', si['scale'])  # Точность (количество значащих цифр после запятой)
+    print('Лот:', si['lot_size'])  # Размер лота
+    print('Шаг цены:', si['min_price_step'])  # Минимальный шаг цены
 
     # Торговый счет тикера
     tradeAccount = qpProvider.GetTradeAccount(classCode)["data"]  # Торговый счет для класса тикера
