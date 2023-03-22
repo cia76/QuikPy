1e82bc483f832f76b03e2431a62b6e55c3152977
Игорь Чечет <ichechet@mail.ru>
2023-03-22 12:08:20 +0500

    Проверка на множественность экземпляров опциональна

--- Examples/01 - Connect.py
+++ Examples/01 - Connect.py
@@ -3,40 +3,34 @@ from datetime import datetime
 from QuikPy import QuikPy  # Работа с QUIK из Python через LUA скрипты QuikSharp
 
 
-def PrintCallback(data):
+def print_callback(data):
     """Пользовательский обработчик события"""
     print(data)  # Печатаем полученные данные
 
 
 if __name__ == '__main__':  # Точка входа при запуске этого скрипта
-    qpProvider = QuikPy()  # Вызываем конструктор QuikPy с подключением к локальному компьютеру с QUIK
-    # qpProvider = QuikPy(Host='<Ваш IP адрес>')  # Вызываем конструктор QuikPy с подключением к удаленному компьютеру с QUIK
-    print(f'Подключено к терминалу QUIK по адресу: {qpProvider.Host}:{qpProvider.RequestsPort},{qpProvider.CallbacksPort}')
+    qp_provider = QuikPy()  # Провайдер работает с запущенным терминалом QUIK
+    # qp_provider2 = QuikPy(Host='<Адрес IP>', RequestsPort='<Порт запросов>', CallbacksPort='<Порт подписок>')  # Для каждого запущенного терминала будет создан свой экземпляр QuikPy
+    # print(f'Экземпляры класса совпадают: {qp_provider2 is qp_provider}')
+    # qp_provider2.CloseConnectionAndThread()
 
-    # QuikPy - Singleton класс. Будет создан 1 экземпляр класса, на него будут все ссылки
-    qpProvider2 = QuikPy()  # QuikPy - это Singleton класс. При попытке создания нового экземпляра получим ссылку на уже имеющийся экземпляр
-    # qpProvider2 = QuikPy(Host='<Ваш IP адрес>')  # Вызываем конструктор QuikPy с подключением к удаленному компьютеру с QUIK
-    print(f'Экземпляры класса совпадают: {qpProvider2 is qpProvider}')
+    # Проверяем соединение
+    print(f'Терминал QUIK подключен к серверу: {qp_provider.IsConnected()["data"] == 1}')
+    print(f'Отклик QUIK на команду Ping: {qp_provider.Ping()["data"]}')  # Проверка работы скрипта QuikSharp. Должен вернуть Pong
 
-    # Проверка соединения
-    print(f'Терминал QUIK подключен к серверу: {qpProvider.IsConnected()["data"] == 1}')
-    print(f'Отклик QUIK на команду Ping: {qpProvider.Ping()["data"]}')  # Проверка работы скрипта QuikSharp. Должен вернуть Pong
-
-    # Сервисные функции
-    d = qpProvider.GetInfoParam('TRADEDATE')['data']  # Дата на сервере в виде строки dd.mm.yyyy
-    t = qpProvider.GetInfoParam('SERVERTIME')['data']  # Время на сервере в виде строки hh:mi:ss
-    dt = datetime.strptime(f'{d} {t}', '%d.%m.%Y %H:%M:%S')  # Переводим строки в дату и время
+    # Проверяем работу запрос/ответ
+    trade_date = qp_provider.GetInfoParam('TRADEDATE')['data']  # Дата на сервере в виде строки dd.mm.yyyy
+    server_time = qp_provider.GetInfoParam('SERVERTIME')['data']  # Время на сервере в виде строки hh:mi:ss
+    dt = datetime.strptime(f'{trade_date} {server_time}', '%d.%m.%Y %H:%M:%S')  # Переводим строки в дату и время
     print(f'Дата и время на сервере: {dt}')
     msg = 'Hello from Python!'
-    print(f'Отправка сообщения в QUIK: {msg}{qpProvider.MessageInfo(msg)["data"]}')  # Проверка работы QUIK. Сообщение в QUIK должно показаться как информационное
+    print(f'Отправка сообщения в QUIK: {msg}{qp_provider.MessageInfo(msg)["data"]}')  # Проверка работы QUIK. Сообщение в QUIK должно показаться как информационное
 
-    # Просмотр изменений состояния соединения терминала QUIK с сервером брокера
-    qpProvider.OnConnected = PrintCallback  # Нажимаем кнопку "Установить соединение" в QUIK
-    qpProvider.OnDisconnected = PrintCallback  # Нажимаем кнопку "Разорвать соединение" в QUIK
-
-    # Просмотр изменений параметров
-    qpProvider.OnParam = PrintCallback  # Текущие параметры изменяются постоянно. Будем их смотреть, пока не нажмем Enter в консоли
+    # Проверяем работу подписок
+    qp_provider.OnConnected = print_callback  # Нажимаем кнопку "Установить соединение" в QUIK
+    qp_provider.OnDisconnected = print_callback  # Нажимаем кнопку "Разорвать соединение" в QUIK
+    qp_provider.OnParam = print_callback  # Текущие параметры изменяются постоянно. Будем их смотреть, пока не нажмем Enter в консоли
 
     # Выход
     input('Enter - выход\n')
-    qpProvider.CloseConnectionAndThread()  # Перед выходом закрываем соединение и поток QuikPy из любого экземпляра
+    qp_provider.CloseConnectionAndThread()  # Перед выходом закрываем соединение и поток QuikPy из любого экземпляра
