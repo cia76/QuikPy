2ff4ee3768a459df3ffc19293ebc16a72d4e7deb
cia76 <ichechet@mail.ru>
2024-08-26 22:52:34 +0500

    Цена за лот/штуку

--- Examples/Ticker.py
+++ Examples/Ticker.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':  # Точка входа при запуске это
 
     # Формат короткого имени для фьючерсов: <Код тикера><Месяц экспирации: 3-H, 6-M, 9-U, 12-Z><Последняя цифра года>. Пример: SiU4, RIU4
     # datanames = ('SBER',)  # Тикер без режима торгов
-    datanames = ('TQBR.SBER', 'TQBR.VTBR', 'SPBFUT.SiU4', 'SPBFUT.RIU4')  # Кортеж тикеров
+    datanames = ('TQBR.SBER', 'SPBFUT.SiU4', 'SPBFUT.RIU4', 'SPBFUT.BRU4', 'SPBFUT.CNYRUBF')  # Кортеж тикеров
 
     for dataname in datanames:  # Пробегаемся по всем тикерам
         class_code, sec_code = qp_provider.dataname_to_class_sec_codes(dataname)  # Код режима торгов и тикер
@@ -24,12 +24,22 @@ if __name__ == '__main__':  # Точка входа при запуске это
         logger.debug(f'Ответ от сервера: {si}')
         logger.info(f'Информация о тикере {si["class_code"]}.{si["sec_code"]} ({si["short_name"]}):')  # Короткое наименование инструмента
         logger.info(f'- Валюта: {si["face_unit"]}')
-        logger.info(f'- Лот: {si["lot_size"]}')
-        logger.info(f'- Шаг цены: {si["min_price_step"]}')
-        logger.info(f'- Кол-во десятичных знаков: {si["scale"]}')
+        lot_size = si['lot_size']  # Лот
+        logger.info(f'- Лот: {lot_size}')
+        min_price_step = si['min_price_step']  # Шаг цены
+        logger.info(f'- Шаг цены: {min_price_step}')
+        scale = si['scale']  # Кол-во десятичных знаков
+        logger.info(f'- Кол-во десятичных знаков: {scale}')
         trade_account = qp_provider.get_trade_account(class_code)['data']  # Торговый счет для класса тикера
         logger.info(f'- Торговый счет: {trade_account}')
         last_price = float(qp_provider.get_param_ex(class_code, sec_code, 'LAST')['data']['param_value'])  # Последняя цена сделки
         logger.info(f'- Последняя цена сделки: {last_price}')
+        step_price = float(qp_provider.get_param_ex(class_code, sec_code, 'STEPPRICE')['data']['param_value'])  # Стоимость шага цены
+        if step_price and lot_size > 1:  # Если есть стоимость шага цены и лот
+            logger.info(f'- Стоимость шага цены: {step_price}')
+            lot_price = last_price / min_price_step * step_price  # Цена за лот
+            logger.info(f'- Цена за лот: {last_price} / {min_price_step} * {step_price} = {lot_price}')
+            pcs_price = lot_price / lot_size  # Цена за штуку
+            logger.info(f'- Цена за штуку: {lot_price} / {lot_size} = {pcs_price}')
 
     qp_provider.close_connection_and_thread()  # Перед выходом закрываем соединение для запросов и поток обработки функций обратного вызова
